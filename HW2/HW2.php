726968b8dd9f91dbbc3b26dca7b6f1f52a3f1ad6
Nikita Orobenko <nikoro99@gmail.com>
2018-08-31 14:58:45 +0300

    Move forward to new logic in code

--- HW2/HW2.php
+++ HW2/HW2.php
@@ -3,5 +3,20 @@ $login=["qwer","wert","rtyu","ghjk"];
 $pass=["xchj","jhkk","jkhk","jhkl"];
 $name=["abdul","mahmul","evgen","tifon"];
 
-$enter_log=$_POST['enter_log'];
+$enter_login=$_POST['enter_login'];
 $enter_pass=$_POST['enter_pass'];
+$login_bool=False;
+$pass_bool=False;
+$index_equality=False;
+for($i=0; $i<count($login); $i++){
+    if ($enter_login==$login[$i]){
+        $login_bool=True;
+        }
+}
+for($i=0; $i<count($login); $i++){
+    if ($enter_pass==$pass[$i]){
+        $pass_bool=True;
+    }
+}
+if ()
+ require "form.php";
---
+++ HW2/form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <style>
+        .colo{
+            color: coral;
+        }
+    </style>
+</head>
+<body>
+<form action="HW2.php" method="post" enctype="multipart/form-data">
+    Login: <input type="text" name="enter_login" /><br>
+    Password: <input type="text" name="enter_pass" /><br>
+    <input type="submit" value="PushPush"/><br>
+    <?php
+    echo "<span class='colo'>$enter_login</span>";
+    ?>
+</form>
+</body>
+</html>
