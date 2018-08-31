861bf24e22660e392b1b8e180bb09ec589e67949
Nikita Orobenko <nikoro99@gmail.com>
2018-08-31 13:03:46 +0300

    Move to another folder

---
+++ HW2/HW2.php
@@ -0,0 +1,7 @@
+<?php
+$login=["qwer","wert","rtyu","ghjk"];
+$pass=["xchj","jhkk","jkhk","jhkl"];
+$name=["abdul","mahmul","evgen","tifon"];
+
+$enter_log=$_POST['enter_log'];
+$enter_pass=$_POST['enter_pass'];
