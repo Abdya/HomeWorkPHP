da4b13aaa5882907532f9f694e694fd7cd231df3
Nikita Orobenko <nikoro99@gmail.com>
2018-09-13 19:37:48 +0300

    HW_3 in process #4

--- HW2/take_data.php
+++ HW2/take_data.php
@@ -2,18 +2,26 @@
 require "common.php";
 $user = $_GET["login"];
 $path = USERS_DIR . "/$user.json";
-$user=find($login);
+#$user=find($login);
 $login_new = $_POST["login"];
 $email = $_POST["email"];
 $name = $_POST["name"];
 $role = $_POST["role"];
 $active = !empty($_POST["active"]);
 
-$user["login"] = $login_new;
-$user["email"] = $email;
-$user["name"] = $name;
-$user["role"] = $role;
-$user["active"] = $active;
+$file = file_get_contents($path);
+$temp = json_decode($file, true);
+
+unset($file);
+$temp[] = array(
+    "login" => $login_new,
+    "email" => $email,
+    "name" => $name,
+    "role" => $role,
+    "active" => $active
+);
+file_put_contents($path, json_encode($temp));
+
 
 rename($path, USERS_DIR . "/$login_new.json");
 header("Location: /HW2/edit_user.php?login=$login_new");
